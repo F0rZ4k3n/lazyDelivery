13421b5b818d71ea29d0a7cd6104ab2deab73a2d
Diru <angeldiazdiru93@gmail.com>
2024-02-04 05:34:29 +0100

    fix module

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express, { response } from "express"
-import { exec } from "child_process"
+import express from "express"
 import fs from "fs"
 import http from "http"
 import https from "https"
 
 const app = express();
 
-const httpsServer = https.createServer(credentials, app);
+//?const httpsServer = https.createServer(credentials, app);
 const httpServer = http.createServer((req, res) => {
     res.writeHead(301, { "Location": `https://${req.headers['host']}${req.url}` });
     res.end();
 });
 
-httpServer.listen(80, () => {
+app.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
+/* httpsServer.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
-})
+}) */
