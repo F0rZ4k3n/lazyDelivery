0ea6800c3028083c21d140cdf5c704076d40a267
Diru <angeldiazdiru93@gmail.com>
2024-02-04 06:58:07 +0100

    finally?

--- index.js
+++ index.js
@@ -1,69 +1,56 @@
-import fs from 'fs'
-import https from 'https'
-import express from 'express'
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+
 const app = express();
 app.use(express.static("public"));
-
-//#region Server Domains and Connections
 
 // Configuración para el Landing Page
 const optionsServer = {
     key: fs.readFileSync('/etc/letsencrypt/live/diru.dev/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/diru.dev/fullchain.pem'),
 };
-  
-const server = https.createServer(optionsServer, (req, res) => {
-    console.log(req.url);
-    if (req.url === '/') {
-        res.writeHead(301, { "Location": `https://${req.headers['host']}${req.url}`});
-        res.end();
-    } else {
-      // Maneja otras rutas aquí, por ejemplo, puedes devolver un 404
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Página no encontrada');
-    }
-});
+
+const server = https.createServer(optionsServer, app);
 
 // Configuración para el WWW
-const credentialsServer = {
-  key: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/fullchain.pem'),
+const credentialsServerWWW = {
+    key: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/fullchain.pem'),
 };
 
-const credentialsWWW = https.createServer(credentialsServer, (req, res) => {
+const serverWWW = https.createServer(credentialsServerWWW, (req, res) => {
     res.end('Hola desde WWW!');
 });
 
 // Configuración para el CV
-const credentialsCV = {
-  key: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/fullchain.pem'),
+const credentialsServerCV = {
+    key: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/fullchain.pem'),
 };
 
-const serverCV = https.createServer(credentialsCV, (req, res) => {
+const serverCV = https.createServer(credentialsServerCV, (req, res) => {
     res.end('Hola desde mi CV!');
 });
 
 // Escuchar en puertos específicos
-const PORT_SERVER = 443
+const PORT_SERVER = 443;
 const PORT_WWW = 444;
-const PORT_CV = 445;  // Puedes usar un puerto diferente para cada subdominio
+const PORT_CV = 445;
 
 server.listen(PORT_SERVER, () => {
     console.log(`Servidor HTTPS para el servidor principal escuchando en el puerto: ${PORT_SERVER}`);
-  });
+});
 
-credentialsWWW.listen(PORT_WWW, () => {
-  console.log(`Servidor HTTPS para WWW escuchando en el puerto: ${PORT_WWW}`);
+serverWWW.listen(PORT_WWW, () => {
+    console.log(`Servidor HTTPS para WWW escuchando en el puerto: ${PORT_WWW}`);
 });
 
 serverCV.listen(PORT_CV, () => {
-  console.log(`Servidor HTTPS para CV escuchando en el puerto: ${PORT_CV}`);
+    console.log(`Servidor HTTPS para CV escuchando en el puerto: ${PORT_CV}`);
 });
 
-//#endregion
-
-  // Rutas para diferentes subdominios
+// Rutas para diferentes subdominios
 app.get('/', (req, res) => {
     res.render("index.ejs");
-  });
+});
