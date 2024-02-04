a047b3ffb28d668d237da7a70c801ac1da7c7ddc
Diru <angeldiazdiru93@gmail.com>
2024-02-04 06:03:28 +0100

    edit certificates

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-import express from "express"
-import fs from "fs"
-import { exec } from "child_process"
-import http from "http"
-import https from "https"
+const https = require('https');
+const fs = require('fs');
 
-const app = express();
-app.use(express.static("public"))
+const optionsServer = {
+    key: fs.readFileSync('/etc/letsencrypt/live/diru.dev/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/diru.dev/fullchain.pem'),
+  };
+  
+  const server = https.createServer(optionsServer, (req, res) => {
+    // Lógica para el primer subdominio
+    res.end('Hola desde el server!');
+  });
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/diru.dev/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/diru.dev/fullchain.pem', 'utf8');
-const credentials = {
-    key: privateKey,
-    cert: certificate
+// Configuración para el primer subdominio
+const optionsSubdominio1 = {
+  key: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.diru.dev/fullchain.pem'),
 };
 
-
-const httpsServer = https.createServer(credentials, app);
-const httpServer = http.createServer((req, res) => {
-    res.writeHead(301, { "Location": `https://${req.headers['host']}${req.url}` });
-    res.end();
+const serverSubdominio1 = https.createServer(optionsSubdominio1, (req, res) => {
+  // Lógica para el primer subdominio
+  res.end('Hola desde WWW!');
 });
 
-app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+// Configuración para el segundo subdominio
+const optionsSubdominio2 = {
+  key: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/cv.diru.dev/fullchain.pem'),
+};
 
-httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
-
-    exec('certbot certonly --webroot -w / -d diru.dev', (err, stdout, stderr) => {
-        if (err) {
-          console.error(`Failed to get CertBot: ${stderr}`);
-        } else {
-          console.log(`Certified Succesfull: \n${stdout}`);
-        }
-      });
+const serverSubdominio2 = https.createServer(optionsSubdominio2, (req, res) => {
+  // Lógica para el segundo subdominio
+  res.end('Hola desde mi CV!');
 });
 
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-})
+// Escuchar en puertos específicos
+const PORT_SERVER = 80
+const PORT_SUBDOMINIO1 = 443;
+const PORT_SUBDOMINIO2 = 444;  // Puedes usar un puerto diferente para cada subdominio
+
+server.listen(PORT_SERVER, () => {
+    console.log(`Servidor HTTPS para el servidor principal escuchando en el puerto: ${PORT_SUBDOMINIO1}`);
+  });
+
+serverSubdominio1.listen(PORT_SUBDOMINIO1, () => {
+  console.log(`Servidor HTTPS para WWW escuchando en el puerto: ${PORT_SUBDOMINIO1}`);
+});
+
+serverSubdominio2.listen(PORT_SUBDOMINIO2, () => {
+  console.log(`Servidor HTTPS para CV escuchando en el puerto: ${PORT_SUBDOMINIO2}`);
+});
