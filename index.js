218169c44b8d7710796bd1f36d4b95c88dba974a
Diru <angeldiazdiru93@gmail.com>
2024-02-04 06:04:19 +0100

    fix dependency

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const https = require('https');
-const fs = require('fs');
+import fs from 'fs'
+import https from 'https'
 
 const optionsServer = {
     key: fs.readFileSync('/etc/letsencrypt/live/diru.dev/privkey.pem'),
